72c4d860f86d085376c2b4359d965991ae03e9d6
SergioAlex2308 <semartinezc23@gmail.com>
2023-03-08 15:58:56 -0500

    Platzi exchange, filters, router, api

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex-col items-center">
+    <h1 class="text-gray-700 text-6xl">Platzi Exchange v1.0.0</h1>
+    <p class="text-gray-600 text-xl text-center">
+      Un proyectaso para obtener data de ciertas cryptos
+      <br />
+      El proyecto pertenece al curso de Vue
+    </p>
+    <router-link to="/" class="mt-5 text-xl text-green-600 hover:underline"
+      >Volver a la página de inicio</router-link
+    >
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <px-assests-table :assets="assets" />
+  </div>
+</template>
+
+<script>
+import api from "@/api";
+import PxAssestsTable from "@/components/PxAssestsTable";
+
+export default {
+  name: "Home",
+  components: { PxAssestsTable },
+
+  data() {
+    return {
+      assets: [],
+    };
+  },
+  created() {
+    api.getAssets().then((assets) => (this.assets = assets));
+  },
+};
+</script>
