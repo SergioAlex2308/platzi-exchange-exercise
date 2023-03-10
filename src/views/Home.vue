0338f1f465a19d7464321a5ec47e454d79d9df97
SergioAlex2308 <semartinezc23@gmail.com>
2023-03-09 20:30:30 -0500

    Loaders vue, get link exchanges, use set to fix reactivity problems

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <px-assests-table :assets="assets" />
+    <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100" />
+    <px-assests-table v-if="!isLoading" :assets="assets" />
   </div>
 </template>
 
@@ -14,11 +15,16 @@ export default {
 
   data() {
     return {
+      isLoading: false,
       assets: [],
     };
   },
   created() {
-    api.getAssets().then((assets) => (this.assets = assets));
+    this.isLoading = true;
+    api
+      .getAssets()
+      .then((assets) => (this.assets = assets))
+      .finally(() => (this.isLoading = false));
   },
 };
 </script>
